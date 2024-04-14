47a82155c6b3c4cf0a082dd8f52688ab63052215
OpeltsUnlimited <patrick@opelt.co.at>
2024-04-14 14:37:00 +0200

    check code and storage of last checksum

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from git_helper import MyRepo
+import json
 
 example_text='''
 Example:
@@ -35,6 +36,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    # pull first
     arcanum_repo = MyRepo("arcanum","https://gitlab.com/mathiashjelm/arcanum/")
     arcanumtesting_repo = MyRepo("arcanumtesting","https://gitlab.com/arcanumtesting/arcanum/")
     if args.pull:
@@ -51,3 +53,69 @@ if __name__ == "__main__":
             pass
     else: # nothing was pulled
         pass
+
+    # load used for check, and make sure minimum data structure exsists
+    last_run = None
+    try:
+        with open('last_run.json', 'r') as file:
+            last_run = json.load(file)
+    except FileNotFoundError:
+        pass # during first run the data might not exsist
+    if  not last_run:
+        last_run = {}
+    if not "local" in last_run:
+        last_run["local"] = {}
+    last_local = last_run["local"]
+    if not "arcanum" in last_local:
+        last_local["arcanum"] = ""
+    if not "testing" in last_local:
+        last_local["testing"] = ""
+    if not "markup" in last_run:
+        last_run["markup"] = {}
+    last_markup = last_run["markup"]
+    if not "arcanum" in last_markup:
+        last_markup["arcanum"] = ""
+    if not "testing" in last_markup:
+        last_markup["testing"] = ""
+
+    # check if a operation was specified:
+    if not args.operation:
+        exit(0)
+
+    # check for lokal:
+    if 'local' in args.operation:
+        if args.check:
+            print(arcanum_repo.getCurrentCommit())
+            if arcanum_repo.getCurrentCommit() != last_local["arcanum"]:
+                args.arcanum=True
+            if arcanumtesting_repo.getCurrentCommit() != last_local["testing"]:
+                args.testing=True
+
+        if args.arcanum:
+            print("Do Arcanum, local")
+            last_local["arcanum"] = arcanum_repo.getCurrentCommit()
+        if args.testing:
+            print("Do Testing, local")
+            last_local["testing"] = arcanumtesting_repo.getCurrentCommit()
+        
+
+    # check for markup
+    if 'markup' in args.operation:
+        if args.check:
+            if arcanum_repo.getCurrentCommit() != last_markup["arcanum"]:
+                args.arcanum=True
+            if arcanumtesting_repo.getCurrentCommit() != last_markup["testing"]:
+                args.testing=True
+
+        if args.arcanum:
+            print("Do Arcanum, local")
+            last_markup["arcanum"] = arcanum_repo.getCurrentCommit()
+        if args.testing:
+            print("Do Testing, local")
+            last_markup["testing"] = arcanumtesting_repo.getCurrentCommit()
+
+    with open('last_run.json', 'w') as file:
+        json.dump(last_run, file)
+
+
+    
